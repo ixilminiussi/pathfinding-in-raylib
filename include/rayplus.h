526faaaf79cd04b7ab230f6d2a9f304e5e5e3b26
ixilminiussi <ixil.miniussi@outlook.fr>
2026-04-11 14:12:06 +0200

    newer cmopatibility issues

--- include/rayplus.h
+++ include/rayplus.h
@@ -3,7 +3,12 @@
 // bad and wrong and do unnecessary copies and use floats for everything and are
 // allergic to operators
 
+#include <raylib.h>
 #include <raymath.h>
+
+// Raylib 5.x+ already defines these operators in raymath.h under __cplusplus.
+// Only define them for older versions to avoid redefinition errors.
+#if RAYLIB_VERSION_MAJOR < 5
 
 // Vector2Add(Vector2 v1, Vector2 v2)
 inline Vector2 operator+(const Vector2 &v1, const Vector2 &v2)
@@ -11,11 +16,11 @@ inline Vector2 operator+(const Vector2 &v1, const Vector2 &v2)
     return {v1.x + v2.x, v1.y + v2.y};
 }
 
-inline bool operator+=(Vector2 &v1, const Vector2 &v2)
+inline const Vector2 &operator+=(Vector2 &v1, const Vector2 &v2)
 {
     v1.x += v2.x;
     v1.y += v2.y;
-    return true;
+    return v1;
 }
 // Vector2Subtract(Vector2 v1, Vector2 v2)
 inline Vector2 operator-(const Vector2 &v1, const Vector2 &v2)
@@ -23,11 +28,11 @@ inline Vector2 operator-(const Vector2 &v1, const Vector2 &v2)
     return {v1.x - v2.x, v1.y - v2.y};
 }
 
-inline bool operator-=(Vector2 &v1, const Vector2 &v2)
+inline const Vector2 &operator-=(Vector2 &v1, const Vector2 &v2)
 {
     v1.x -= v2.x;
     v1.y -= v2.y;
-    return true;
+    return v1;
 }
 // Vector2Equals(Vector2 p, Vector2 q)
 inline bool operator==(const Vector2 &v1, const Vector2 &v2)
@@ -45,9 +50,11 @@ inline Vector2 operator*(const Vector2 &v, float scale)
     return {v.x * scale, v.y * scale};
 }
 
-inline bool operator*=(Vector2 &v, float scale)
+inline const Vector2 &operator*=(Vector2 &v, float scale)
 {
     v.x *= scale;
     v.y *= scale;
-    return true;
+    return v;
 }
+
+#endif // RAYLIB_VERSION_MAJOR < 5
